928b75813afa55ffe8505de5f4e5cd6d493ed048
Alexandre Silva <xande12312231@hotmail.com>
2019-02-04 16:41:18 -0200

    Fix

---
+++ api/joingroup.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: application/json');
+?>
+
+{"code": "AyrYv8", "guild": {"verification_level": 0, "features": [], "name": "Web Discord", "splash": null, "id": "539948404042629122", "icon": "5d1e6a9f983d4c8852ba1755d117310b"}, "approximate_member_count": 125, "approximate_presence_count": 36, "inviter": {"username": "kaway404", "discriminator": "8814", "id": "477510907187036180", "avatar": "c7c76545e56a7b8d926192e756ffd06b"}, "channel": {"type": 0, "id": "539948404042629124", "name": "bate-papo"}}
