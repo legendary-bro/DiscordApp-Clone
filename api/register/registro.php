f089fa332b69b31d1dc4173ae02a8f0f93e65fe3
Alexandre Silva <xande12312231@hotmail.com>
2019-02-05 16:34:47 -0200

    Fix

--- api/register/registro.php
+++ api/register/registro.php
@@ -4,26 +4,15 @@ $postdata = file_get_contents("php://input");
 $obj = json_decode($postdata);
 $email = $obj->{'email'};
 $senha =$obj->{'password'};
-if(empty($email)){
-	header("HTTP/1.1 400 Bad Request");
-	echo '{"email": ["Este campo \u00e9 obrigat\u00f3rio"], "password": ["Este campo \u00e9 obrigat\u00f3rio"]}';
-}
-else{
-	if(empty($email)){
-		header("HTTP/1.1 400 Bad Request");
-		echo '{"email": ["Este campo \u00e9 obrigat\u00f3rio"]}';
-	}
-	else if(empty($senha)){
-		header("HTTP/1.1 400 Bad Request");
-		echo '{"password": ["Este campo \u00e9 obrigat\u00f3rio"]}';
-	}
-	else if(isset($email) && isset($senha)){
+$username =$obj->{'username'};
+
 		header("HTTP/1.1 200 OK");
 		// $token = 'NDc3NTEwOTA3MTg3MDM2MTgw.DzsuAg.KB01HjRHEguGlUd8omq5JYxVjks';
 		// echo '{"token": "' . $token . '"}';
-		$data = array("email" => $email, "password" => $senha);                                                                    
+		$data = array("email" => $email, "password" => $senha, "username" => $username);                                                                    
 		$data_string = json_encode($data);                                                                                   
-		$ch = curl_init('https://discordapp.com/api/v6/auth/login');                                                                     
+
+		$ch = curl_init('https://discordapp.com/api/v6/auth/register');                                                                     
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
@@ -31,21 +20,46 @@ else{
 		    'Content-Type: application/json',                                                                                
 		    'Content-Length: ' . strlen($data_string))                                                                       
 		);                                                                                                                   
+		header("HTTP/1.1 400 Bad Request");
 		$answer  = curl_exec($ch);
-		if($answer == '{"password": ["Password does not match."]}'){
-			header("HTTP/1.1 400 Bad Request");
-			echo '{"password": ["Senha incorreta!"]}';
+		if($answer == '{"email": ["This field is required"], "password": ["This field is required"], "username": ["This field is required"]}'){
+			echo '{"email": ["This field is required"], "password": ["This field is required"], "username": ["This field is required"]}';
+		}
+		else if($answer == '{"password": ["This field is required"], "username": ["This field is required"]}'){
+			echo '{"password": ["This field is required"], "username": ["This field is required"]}';
+		}
+		else if($answer == '{"password": ["This field is required"]}'){
+			echo '{"password": ["This field is required"]}';
+		}
+		else if($answer == '{"password": ["Must be between 6 and 128 in length."]}'){
+			echo '{"password": ["Must be between 6 and 128 in length."]}';
+		}
+		else if($answer == '{"email": ["Email is already registered."]}'){
+			echo '{"email": ["Email is already registered."]}';
+		}
+		else if($answer == '{
+  "global": false, 
+  "message": "You are being rate limited.", 
+  "retry_after": 26951
+}
+'){
+			echo '{
+  "global": false, 
+  "message": "You are being rate limited.", 
+  "retry_after": 26951
+}
+';
 		}
 		else if($answer == '{"captcha_key": ["captcha-required"]}'){
-			header("HTTP/1.1 400 Bad Request");
 			echo '{"captcha_key": ["captcha-required"]}';
 		}
 		else{
+		header("HTTP/1.1 200 Ok");
 		echo $answer;
 		}
+
 		if (curl_error($ch)) {
 		    echo curl_error($ch);
 		}
-	}
-}
+
 ?>
--- api/register/required.php
+++ api/register/required.php
@@ -4,26 +4,14 @@ $postdata = file_get_contents("php://input");
 $obj = json_decode($postdata);
 $email = $obj->{'email'};
 $senha =$obj->{'password'};
-if(empty($email)){
-	header("HTTP/1.1 400 Bad Request");
-	echo '{"email": ["Este campo \u00e9 obrigat\u00f3rio"], "password": ["Este campo \u00e9 obrigat\u00f3rio"]}';
-}
-else{
-	if(empty($email)){
-		header("HTTP/1.1 400 Bad Request");
-		echo '{"email": ["Este campo \u00e9 obrigat\u00f3rio"]}';
-	}
-	else if(empty($senha)){
-		header("HTTP/1.1 400 Bad Request");
-		echo '{"password": ["Este campo \u00e9 obrigat\u00f3rio"]}';
-	}
-	else if(isset($email) && isset($senha)){
+
 		header("HTTP/1.1 200 OK");
 		// $token = 'NDc3NTEwOTA3MTg3MDM2MTgw.DzsuAg.KB01HjRHEguGlUd8omq5JYxVjks';
 		// echo '{"token": "' . $token . '"}';
 		$data = array("email" => $email, "password" => $senha);                                                                    
 		$data_string = json_encode($data);                                                                                   
-		$ch = curl_init('https://discordapp.com/api/v6/auth/login');                                                                     
+
+		$ch = curl_init('https://discordapp.com/api/v6/auth/consent-required');                                                                   
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
@@ -31,21 +19,18 @@ else{
 		    'Content-Type: application/json',                                                                                
 		    'Content-Length: ' . strlen($data_string))                                                                       
 		);                                                                                                                   
+
 		$answer  = curl_exec($ch);
-		if($answer == '{"password": ["Password does not match."]}'){
+		if($answer == '{"email": ["This field is required"], "password": ["This field is required"], "username": ["This field is required"]}'){
 			header("HTTP/1.1 400 Bad Request");
-			echo '{"password": ["Senha incorreta!"]}';
-		}
-		else if($answer == '{"captcha_key": ["captcha-required"]}'){
-			header("HTTP/1.1 400 Bad Request");
-			echo '{"captcha_key": ["captcha-required"]}';
+			echo '{"email": ["This field is required"], "password": ["This field is required"], "username": ["This field is required"]}';
 		}
 		else{
 		echo $answer;
 		}
+
 		if (curl_error($ch)) {
 		    echo curl_error($ch);
 		}
-	}
-}
+
 ?>
